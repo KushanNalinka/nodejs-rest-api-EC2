e58e3f9e2cb6cec4e91a1675cd4418f0ddf78e3f
Kushan Nalinka <kushannalinka572@gmail.com>
2025-02-26 01:20:59 +0530

    Port chane

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use("/api/users", userRoutes);
 app.use(errorHandler);
 
 // Start Server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 });
